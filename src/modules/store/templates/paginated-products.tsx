fb7d18953039a96eee72c7634e7e34bc0936aaec
aban0215 <sunming257@gmail.com>
2026-01-09 18:00:52 +0800

    add filter6666666666

--- src/modules/store/templates/paginated-products.tsx
+++ src/modules/store/templates/paginated-products.tsx
@@ -67,14 +67,18 @@ export default async function PaginatedProducts({
         queryParams["tag_id"] = [material]
     }
 
-// 2. 【规格过滤】因为 3 层深度被封死了，V2 Store API 提供了一个扁平化入口
     if (size || color) {
-        const opts: string[] = []
-        if (size) opts.push(size)
-        if (color) opts.push(color)
+        const activeOptions: string[] = []
+        if (size) activeOptions.push(size)
+        if (color) activeOptions.push(color)
 
-        // 方案 B：不再深入嵌套，改用 V2 专门为 Store 端设计的扁平 key
-        queryParams["option_value"] = opts
+        if (activeOptions.length > 0) {
+            queryParams.variants = {
+                options: {
+                    value: activeOptions
+                }
+            }
+        }
     }
 
     if (sortBy === "created_at") {

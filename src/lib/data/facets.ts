c8615d96de2fb9de25336c2b79849164144cd350
aban0215 <sunming257@gmail.com>
2026-01-10 16:54:00 +0800

    2

--- src/lib/data/facets.ts
+++ src/lib/data/facets.ts
@@ -1,3 +1,4 @@
+import {isServer} from "@stripe/react-stripe-js/src/utils/isServer";
 
 export type FacetSnapshot = {
     id: string
@@ -14,7 +15,12 @@ export type FacetSnapshot = {
  * 从 MeiliSearch 获取指定分类或系列的属性快照
  */
 export async function getFacetSnapshot(id: string): Promise<FacetSnapshot | null> {
-    const host = process.env.NEXT_PUBLIC_MEILISEARCH_HOST
+    // const host = process.env.NEXT_PUBLIC_MEILISEARCH_HOST
+
+    const host = isServer
+        ? "http://127.0.0.1:7700"  // 服务端走本地
+        : process.env.NEXT_PUBLIC_MEILISEARCH_HOST // 客户端走公网
+
     const apiKey = process.env.NEXT_PUBLIC_MEILISEARCH_API_KEY
 
     if (!host || !apiKey) {

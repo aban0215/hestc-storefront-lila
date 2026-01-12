7d57893ac2e8d4b328f3bb26de3b2f28ae74ef57
aban0215 <sunming257@gmail.com>
2026-01-12 14:50:05 +0800

    过滤接口调试8

--- src/lib/data/products.ts
+++ src/lib/data/products.ts
@@ -48,8 +48,7 @@ export const listProducts = async ({
     offset: 0,
     region_id: region?.id,
     order: order,
-    // fields: "title,handle,description,material,*variants.calculated_price,+variants.inventory_quantity,*variants.images,+metadata,+tags,+variants.options,+variants.options.option,+collection",
-    fields: "material,*variants.calculated_price,+variants.inventory_quantity,*variants.images,+metadata,+tags,+variants.options,+variants.options.option,+collection",
+    fields: "*variants.calculated_price,+variants.inventory_quantity,*variants.images,+metadata,+tags,+material,+variants.options,+variants.options.option,+collection",
   }
 
   if (category_id) {
